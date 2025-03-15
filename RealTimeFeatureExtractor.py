4c3e8e6e5cc17c36ffc1d3306cd04091e6dbc409
DanielFalkner <da.falkner@gmx.at>
2025-03-15 13:29:54 +0100

    deleted method in RealTimeFeatureExtractor because it was not used anymore

--- RealTimeFeatureExtractor.py
+++ RealTimeFeatureExtractor.py
@@ -58,12 +58,14 @@ class RealTimeFeatureExtractor:
             srcMAC = getattr(packet, 'src', '00:00:00:00:00:00')
             dstMAC = getattr(packet, 'dst', '00:00:00:00:00:00')
 
+            """
             # Nur Pakete weiterverarbeiten, die für das Gerät bestimmt sind
             # Entweder so lassen oder das ganze Netzwerk analysieren
             print(f"Target-IP: {self.targetIP}, Empfangenes Paket für: {dstIP}")
             if dstIP != self.targetIP:
                 print("Paket nicht für Target-IP. Übersprungen.")
                 return None
+            """
 
             return self.nstat.updateGetStats(
                 str(IPtype), srcMAC, dstMAC, srcIP, str(srcproto), dstIP, str(dstproto),
