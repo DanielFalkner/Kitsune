2dc6208c353dc909411c04b9fd3430b6dec517fb
DanielFalkner <da.falkner@gmx.at>
2025-05-07 14:27:26 +0200

    changed train() method, so it works with a pre-existing Feature Mapper

--- KitNET/KitNET.py
+++ KitNET/KitNET.py
@@ -4,22 +4,24 @@ import KitNET.corClust as CC
 import json
 import os
 
+
 # This class represents a KitNET machine learner.
 # KitNET is a lightweight online anomaly detection algorithm based on an ensemble of autoencoders.
 # For more information and citation, please see our NDSS'18 paper: Kitsune: An Ensemble of Autoencoders for Online Network Intrusion Detection
 # For licensing information, see the end of this document
 
 class KitNET:
-    #n: the number of features in your input dataset (i.e., x \in R^n)
-    #m: the maximum size of any autoencoder in the ensemble layer
-    #AD_grace_period: the number of instances the network will learn from before producing anomaly scores
-    #FM_grace_period: the number of instances which will be taken to learn the feature mapping. If 'None', then FM_grace_period=AM_grace_period
-    #learning_rate: the default stochastic gradient descent learning rate for all autoencoders in the KitNET instance.
-    #hidden_ratio: the default ratio of hidden to visible neurons. E.g., 0.75 will cause roughly a 25% compression in the hidden layer.
-    #feature_map: One may optionally provide a feature map instead of learning one. The map must be a list,
+    # n: the number of features in your input dataset (i.e., x \in R^n)
+    # m: the maximum size of any autoencoder in the ensemble layer
+    # AD_grace_period: the number of instances the network will learn from before producing anomaly scores
+    # FM_grace_period: the number of instances which will be taken to learn the feature mapping. If 'None', then FM_grace_period=AM_grace_period
+    # learning_rate: the default stochastic gradient descent learning rate for all autoencoders in the KitNET instance.
+    # hidden_ratio: the default ratio of hidden to visible neurons. E.g., 0.75 will cause roughly a 25% compression in the hidden layer.
+    # feature_map: One may optionally provide a feature map instead of learning one. The map must be a list,
     #           where the i-th entry contains a list of the feature indices to be assingned to the i-th autoencoder in the ensemble.
     #           For example, [[2,5,3],[4,0,1],[6,7]]
-    def __init__(self,n,max_autoencoder_size=10,FM_grace_period=None,AD_grace_period=10000,learning_rate=0.1,hidden_ratio=0.75, feature_map = None):
+    def __init__(self, n, max_autoencoder_size=10, FM_grace_period=None, AD_grace_period=10000, learning_rate=0.1,
+                 hidden_ratio=0.75, feature_map=None):
         # Parameters:
         self.AD_grace_period = AD_grace_period
         if FM_grace_period is None:
@@ -35,8 +37,8 @@ class KitNET:
         self.n = n
 
         # Variables
-        self.n_trained = 0 # the number of training instances so far
-        self.n_executed = 0 # the number of executed instances so far
+        self.n_trained = 0  # the number of training instances so far
+        self.n_executed = 0  # the number of executed instances so far
         self.v = feature_map
         if self.v is None:
             feature_map_path = "fixed_feature_map.json"
@@ -50,17 +52,17 @@ class KitNET:
         self.ensembleLayer = []
         self.__createAD__()
         print("Feature-Mapper: execute-mode, Anomaly-Detector: train-mode")
-        self.FM = CC.corClust(self.n) #incremental feature cluatering for the feature mapping process
+        self.FM = CC.corClust(self.n)  # incremental feature cluatering for the feature mapping process
         self.outputLayer = None
 
-    #If FM_grace_period+AM_grace_period has passed, then this function executes KitNET on x. Otherwise, this function learns from x.
-    #x: a numpy array of length n
-    #Note: KitNET automatically performs 0-1 normalization on all attributes.
-    def process(self,x):
+    # If FM_grace_period+AM_grace_period has passed, then this function executes KitNET on x. Otherwise, this function learns from x.
+    # x: a numpy array of length n
+    # Note: KitNET automatically performs 0-1 normalization on all attributes.
+    def process(self, x):
         try:
             print(f"[DEBUG] Training Counter: {self.n_trained}")
             print(f"[DEBUG] FM Grace Period: {self.FM_grace_period}, AD Grace Period: {self.AD_grace_period}")
-            if self.n_trained > self.FM_grace_period + self.AD_grace_period: #If both the FM and AD are in execute-mode
+            if self.n_trained > self.FM_grace_period + self.AD_grace_period:  # If both the FM and AD are in execute-mode
                 print("[DEBUG] In Execute Mode")
                 rmse = self.execute(x)
                 print(f"[DEBUG] RMSE (Execute Mode): {rmse}")
@@ -74,6 +76,7 @@ class KitNET:
             print(f"[KitNET-Fehler in process()]: {e}")
             return -1
 
+    """
     #force train KitNET on x
     #returns the anomaly score of x during training (do not use for alerting)
     def train(self,x):
@@ -99,11 +102,41 @@ class KitNET:
                 from edgeDevice import device_id
                 self.log_feature_map(device_id)
         self.n_trained += 1
+    """
 
-    #force execute KitNET on x
-    def execute(self,x):
+    def train(self, x):
+        if self.v is None:  # Wenn keine Feature-Map existiert, Feature-Mapping durchführen
+            if self.n_trained <= self.FM_grace_period:  # FM-Phase
+                self.FM.update(x)
+                if self.n_trained == self.FM_grace_period:  # Wenn FM-Phase abgeschlossen
+                    self.v = self.FM.cluster(self.m)
+                    self.__createAD__()
+                    print("The Feature-Mapper found a mapping: " + str(self.n) + " features to " + str(
+                        len(self.v)) + " autoencoders.")
+                    print("Feature-Mapper: execute-mode, Anomaly-Detector: train-mode")
+        else:
+            # Training der Autoencoder mit der festen Feature-Map
+            S_l1 = np.zeros(len(self.ensembleLayer))
+            for a in range(len(self.ensembleLayer)):
+                xi = x[self.v[a]]
+                S_l1[a] = self.ensembleLayer[a].train(xi)
+
+            # Training des Output-Layers
+            if self.outputLayer is not None:
+                self.outputLayer.train(S_l1)
+
+            if self.n_trained == self.AD_grace_period + self.FM_grace_period:
+                print("Feature-Mapper: execute-mode, Anomaly-Detector: execute-mode")
+                from edgeDevice import device_id
+                self.log_feature_map(device_id)
+
+        self.n_trained += 1
+
+    # force execute KitNET on x
+    def execute(self, x):
         if self.v is None:
-            raise RuntimeError('KitNET Cannot execute x, because a feature mapping has not yet been learned or provided. Try running process(x) instead.')
+            raise RuntimeError(
+                'KitNET Cannot execute x, because a feature mapping has not yet been learned or provided. Try running process(x) instead.')
         else:
             self.n_executed += 1
             ## Ensemble Layer
@@ -118,11 +151,13 @@ class KitNET:
     def __createAD__(self):
         # construct ensemble layer
         for map in self.v:
-            params = AE.dA_params(n_visible=len(map), n_hidden=0, lr=self.lr, corruption_level=0, gracePeriod=0, hiddenRatio=self.hr)
+            params = AE.dA_params(n_visible=len(map), n_hidden=0, lr=self.lr, corruption_level=0, gracePeriod=0,
+                                  hiddenRatio=self.hr)
             self.ensembleLayer.append(AE.dA(params))
 
         # construct output layer
-        params = AE.dA_params(len(self.v), n_hidden=0, lr=self.lr, corruption_level=0, gracePeriod=0, hiddenRatio=self.hr)
+        params = AE.dA_params(len(self.v), n_hidden=0, lr=self.lr, corruption_level=0, gracePeriod=0,
+                              hiddenRatio=self.hr)
         self.outputLayer = AE.dA(params)
 
     def log_feature_map(self, device_id="default", output_dir="Logs"):
